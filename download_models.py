e367e02704f01a100982c7c39dee4df63549461e
cKeng <8955272+ckeng@user.noreply.gitee.com>
2025-10-24 17:13:54 +0800

    f

--- download_models.py
+++ download_models.py
@@ -1,13 +1,13 @@
 # download_models.py
-import os
 from speechbrain.inference.classifiers import EncoderClassifier
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+import os
 
 # 确保缓存目录存在
 hf_home = os.environ.get("HF_HOME", "/app/huggingface_cache")
 os.makedirs(hf_home, exist_ok=True)
 
-print("开始预下载所有模型...")
+
+print("开始预下载声纹提取模型...")
 
 try:
     EncoderClassifier.from_hparams(
@@ -17,13 +17,3 @@ try:
     print("✅ 声纹提取模型下载完毕。")
 except Exception as e:
     print(f"🔴 声纹提取模型下载失败: {e}")
-
-try:
-    SpeechT5Processor.from_pretrained("microsoft/speecht5_tts", language="zh-cn")
-    SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-    SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
-    print("✅ TTS 相关模型下载完毕。")
-except Exception as e:
-    print(f"🔴 TTS 模型下载失败: {e}")
-
-print("✅ 所有模型预下载完成！")
